a2af35cc5e6091d1c20b37dc01426c0962992640
Maria Eduarda Moreira <maddum.santos24@gmail.com>
2024-11-03 19:56:33 -0300

    primeiro commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { NavigationContainer} from '@react-navigation/native'
+import { Routes } from '../(tabs)/routes'
+
+
+export default function Index(){
+  
+  return(
+    <NavigationContainer independent={true}>
+      <Routes>
+
+      </Routes>
+    </NavigationContainer>
+  )
+}
